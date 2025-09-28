fc3c9790c11ca0f420b6f36d6302031e63d96208
Gerardo <gerardoalonso.martinezmonge@gmail.com>
2025-09-28 09:18:18 -0600

    feat: implement HeroesLayout and AdminLayout components with routing

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,6 +1,25 @@
+import { Outlet, Link } from "react-router"
 
 export const HeroesLayout = () => {
-  return (
-    <div>HeroesLayout</div>
-  )
+    return (
+        <div className="bg-red-500">
+
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/heroes/1'>Hero</Link>
+                </li>
+                <li>
+                    <Link to='/search'>Busqueda</Link>
+                </li>
+                <li>
+                    <Link to='/admin'>Admin</Link>
+                </li>
+            </ul>
+
+            <Outlet />
+        </div>
+    )
 }
