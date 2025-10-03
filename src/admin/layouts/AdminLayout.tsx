d794f5b1690a43f89d1c5f2d03fca877b119f849
Gerardo <gerardoalonso.martinezmonge@gmail.com>
2025-10-03 07:14:14 -0600

    Refactor: The homepage was broken down into reusable components.

--- src/admin/layouts/AdminLayout.tsx
+++ src/admin/layouts/AdminLayout.tsx
@@ -2,8 +2,11 @@ import { Outlet } from "react-router"
 
 export const AdminLayout = () => {
     return (
-        <div className="bg-emerald-700">
-            <Outlet />
+
+        <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-purple-50">
+            <div className="max-w-7xl mx-auto p-6">
+                <Outlet />
+            </div>
         </div>
     )
 }
--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,7 +1,19 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControl } from "./ui/SearchControl";
 
 export const SearchPage = () => {
   return (
-    <div>SearchPage</div>
+    <>
+      <CustomJumbotron
+        title="Busqueda de SuperHeroes"
+        description="Descubre, explora y administra tus superheroes y villanos favoritos"
+      />
+
+      <HeroStats />
+
+      <SearchControl/>
+    </>
   )
 }
 
